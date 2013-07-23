80b9a5fa9d306d81536eda7fcc35d87b2a652921
vrushali12 <vrushali12dhivare@gmail.com>
2013-07-23 18:05:24 +0530

    ngl

---
+++ dompdf/brylle_try.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+<title>Patron Barcodes (8.5 X 11)</title>
+
+	<link href="../bcodes.css" rel="stylesheet" type="text/css" />
+
+<style type="text/css">
+
+	body {
+
+		background-color:#EEE;
+
+		font-size:12px;
+
+	}
+
+	table {
+
+		width:8.5in;
+
+		background-color:#FFF;
+
+	}
+
+	td {
+
+		width:2.8333333333333in;
+
+		height:1.5714285714286in;
+
+		vertical-align:top;
+
+		border:thin dashed #CCC;
+
+		border-radius:5px;
+
+		padding:5px;
+
+	}
+
+	img {
+
+		width:100%;
+
+		height:50%;	
+
+	}
+
+</style>
+
+</head>
+
+
+
+<body>
+
+<div align="center">
+
+<table cellspacing="0" >
+
+	<tr>
+
+  <tr><td align='center' width='2.8333333333333' height='1.5714285714286'><label><b>MARYKNOLL HIGH SCHOOL OF PANABO Library</b></label>
+
+				<img src='http://localhost/oll/admin/php_bcodes/genbcodesimgsrc/images/1200001173.png' alt='1200001173' />
+
+				<label>1200001173</label><br />
+
+				<label>ABUCAY, JENEBI L.</label></td><td align='center' width='2.8333333333333' height='1.5714285714286'><label><b>MARYKNOLL HIGH SCHOOL OF PANABO Library</b></label>
+
+				<img src='http://localhost/oll/admin/php_bcodes/genbcodesimgsrc/images/1200001174.png' alt='1200001174' />
+
+				<label>1200001174</label><br />
+
+				<label>ASOY, MARJORIE L.</label></td><td align='center' width='2.8333333333333' height='1.5714285714286'><label><b>MARYKNOLL HIGH SCHOOL OF PANABO Library</b></label>
+
+				<img src='http://localhost/oll/admin/php_bcodes/genbcodesimgsrc/images/1200001175.png' alt='1200001175' />
+
+				<label>1200001175</label><br />
+
+				<label>SERASPE, BRYLLE D.</label></td></tr>  </tr>
+
+</table>
+
+</div>
+
+</body>
+
+</html>
+
+
+
---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title></title>
+<link rel="stylesheet" type="text/css" href="./css/style.css" />
+</head>
+
+<body>
+<div class="container">
+  <div class="rules">
+  <ol>
+  <li>All the result of the report prepared by gemologist of Navratna Gemological Laboratory to the best of their knowledge.</li>
+  </li>
+  <li>No futher modifications & changes of report by client request.</li>
+  <li>All the results & Calculations of the report are depends upon accuracy of instruments it may be varying with techniques of grmologist & instruments.</li>
+  <li>The Liability of  Navratna gemological laboratory as regards to any claim for any damage to the article(s) directly or indirectly by the client/owner of the article(s) shall be limited to the amount received as fees in connection to the report</li>
+  <li>All Maters Disputes or references to arbitration arising out of this report to the jurisdiction Nashik.<br />The report shall be bound by the Indian judicial systems.</li>
+  
+  <li>This report is neither an appraisal nor any guarantee/warranty of the article(s) referred to in this report.</li>
+  <li>Any Doubt or mistake of this report please shows report & clear doubts.</li>
+  <li>Collect your stone at that time when report collect...It's your responsibility.</li>
+  </ol>
+  </div>
+   <div class="heading">
+  	<h3><font color="#CC0000" size="+1">N</font>AVRATNA <font color="#CC0000" size="+2">G</font>EMOLOGICAL <font color="#CC0000" size="+2">L</font>ABORATORY &#0153;</h3>
+    <p>1<sup>st</sup> Floor,Alankar Market,Opp.Okachit talim,Jijamata road,Saraf Bazaar,Nasik-422001</p>
+    <p>MOB-09923051515,09823654220,9545901515 RES-0253-2506515</p>
+    <p>E-mail:-<a href="#">bhavehgems@gmail.com,bsvispute@gmail.com</a></p>
+    <p>POST YOUR COMMENTS ON <a href="#">WWW.FACEBOOK.COM/BHAVESH.VISPUTE</a></p>
+    <p><font color="#003399" size="+1">www.gemslab.in</font></p>
+    </div>
+</div>
+</body>
+</html>
+
+<?php /*?><?php
+   $htmlcontent=ob_get_clean();
+   
+   include("dompdf/dompdf_config.inc.php");
+  $htmlcontent = stripslashes($htmlcontent);
+  $dompdf = new DOMPDF();
+  $dompdf->load_html($htmlcontent);
+  $dompdf->set_paper("folio", "portrait");
+  $dompdf->render();
+  $dompdf->stream($filename, array("Attachment" => false));	
+  exit(0);
+?>
+<?php */?>
